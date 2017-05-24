f5dd5b6d830742ef8e4aaae94bf86e8fb534da8d
Artem Pastukhov <pastukhov@lecta.ru>
2017-05-24 12:15:34 +0300

    Polish

--- readConfig.lua
+++ readConfig.lua
@@ -2,7 +2,7 @@ if file.exists("config.json") then
     file.open("config.json","r")
     config = sjson.decode(file.read())
     file.close()
-    print(sjson.encode(config))
+--    print(sjson.encode(config))
 else
     print("Upload config.json!")
 end
