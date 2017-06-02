8a51be72545fb5155de943aa484354118e576eed
Artem Pastukhov <pastukhov@lecta.ru>
2017-06-02 14:51:16 +0300

    Fixes

--- initDisplay.lua
+++ initDisplay.lua
@@ -1,8 +1,8 @@
 local sda = 2 -- GPIO14
 local scl = 1 -- GPIO12
 local sla = 0x3c -- 0x3c or 0x3d
-i2c.setup(0, sda, scl, i2c.SLOW)
-disp = u8g.ssd1306_64x48_i2c(sla)
+print(i2c.setup(0, sda, scl, i2c.SLOW))
+disp = u8g.ssd1306_64x48_i2c(sla,0)
     
 disp:firstPage()
 repeat
