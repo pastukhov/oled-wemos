7a68ddf04ba2f8eb8bf577ce46f20914a1e60776
Artem Pastukhov <pastukhov@lecta.ru>
2017-06-06 20:00:42 +0300

    Moved to init.d

--- initDisplay.lua
+++
@@ -1,9 +0,0 @@
-local sda = 2 -- GPIO14
-local scl = 1 -- GPIO12
-local sla = 0x3c -- 0x3c or 0x3d
-print(i2c.setup(0, sda, scl, i2c.SLOW))
-disp = u8g.ssd1306_64x48_i2c(sla,0)
-    
-disp:firstPage()
-repeat
-until disp:nextPage() == false
--- readConfig.lua
+++
@@ -1,8 +0,0 @@
-if file.exists("config.json") then
-    file.open("config.json","r")
-    config = sjson.decode(file.read())
-    file.close()
-    if debug then print(sjson.encode(config)) end
-else
-    print("Upload config.json!")
-end
